9dbcd25a9c8dde75f5bc6e6253a73056387ecc73
mattin <mattinmir@gmail.com>
2015-02-20 22:05:13 +0000

    Only q2 and q4 left

--- db_2015_cw3_template.sql
+++ db_2015_cw3_template.sql
@@ -29,29 +29,52 @@ ORDER BY name
 ; 
 
 -- Q4 returns (father,mother,child,born)------------------------------------------------------todo
-SELECT father, mother, name AS child, RANK(dob)
+
+--Gives correct except rank is over all families not separate families
+SELECT father, mother, name AS child, rank() over (ORDER BY dob) AS born 
 FROM person
-WHERE (mother IS NOT NULL OR father IS NOT NULL)
+WHERE mother IS NOT NULL OR father IS NOT NULL
+GROUP BY mother, father, name
+ORDER BY father, mother, born;
+
+
+
+-- Q5 returns (father,mother,male)
+SELECT mother, father,
+	-- Taking advantage of the fact that COUNT doesn't count null values
+	 CAST(COUNT(CASE gender WHEN 'M' THEN 1 ELSE NULL END) * 1.0 / COUNT(gender) * 100.0 AS integer) AS male
+FROM person WHERE mother IS NOT NULL AND father IS NOT NULL
+GROUP BY mother, father
+ORDER BY mother, father;
+ 
+
+-- Q6 returns (name)
+SELECT mother AS name
+FROM
+	(
+	SELECT mother, COUNT(gender) 
+	FROM person
+	WHERE mother IS NOT NULL 
+	OR father IS NOT NULL
+	GROUP BY mother, gender
+	) AS child_gender_count
+GROUP BY mother
+HAVING COUNT(mother) = 1
+
+UNION
+
+SELECT father AS name
+FROM
+	(
+	SELECT father,COUNT(gender) 
+	FROM person
+	WHERE mother IS NOT NULL 
+	OR father IS NOT NULL
+	GROUP BY father, gender
+	) AS child_gender_count
+GROUP BY father
+HAVING COUNT(father) = 1
+
+ORDER BY NAME
 ;
 
---------------------
---Returns all children with at least one parent
-SELECT father, mother, name AS child, dob
-FROM person                                
-WHERE mother IS NOT NULL
-OR father IS NOT NULL
-ORDER BY father, mother;
-
-
--- Q5 returns (father,mother,male)--------------------------------------------------------todo
-WHERE (SELECT DISTINCT gender FROM person) = 'M'
-;
-
--- Q6 returns (name)----------------------------------------------------------------------todo
-WHERE COUNT(SELECT DISTINCT gender FROM person) = 1
-;
-
-SELECT ALL name, ALL father, ALL mother, DISTINCT gender
-FROM person AS families
-WHERE (father IS NOT NULL OR mother IS NOT NULL)
-AND 
