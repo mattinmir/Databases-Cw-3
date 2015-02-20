f1eec129679d439f2155fd390a4d74755184c0e6
mattin <mattinmir@gmail.com>
2015-02-20 23:03:19 +0000

    First working draft

--- db_2015_cw3_template.sql
+++ db_2015_cw3_template.sql
@@ -12,13 +12,29 @@ HAVING COUNT(names) > 1
 ORDER BY popularity DESC, names
 ;
 
--- Q2 returns (name,forties,fifties,sixties)----------------------------------------------------todo
-SELECT 	mother,  
-	EXTRACT(YEAR FROM dob)
+-- Q2 returns (name,forties,fifties,sixties)
+SELECT mother AS name,  
+	COUNT(CASE FLOOR(EXTRACT(YEAR FROM dob) / 10) * 10 WHEN 1940 THEN 1 ELSE NULL END) AS  fourties,
+	COUNT(CASE FLOOR(EXTRACT(YEAR FROM dob) / 10) * 10 WHEN 1950 THEN 1 ELSE NULL END) AS  fifties,
+	COUNT(CASE FLOOR(EXTRACT(YEAR FROM dob) / 10) * 10 WHEN 1960 THEN 1 ELSE NULL END) AS  sixties
 FROM person
 WHERE mother IS NOT NULL
-GROUP BY mother, dob
-HAVING COUNT(name) > 1;
+GROUP BY mother
+HAVING COUNT(name) > 1
+
+UNION	
+
+SELECT father AS name,  
+	COUNT(CASE FLOOR(EXTRACT(YEAR FROM dob) / 10) * 10 WHEN 1940 THEN 1 ELSE NULL END) AS  fourties,
+	COUNT(CASE FLOOR(EXTRACT(YEAR FROM dob) / 10) * 10 WHEN 1950 THEN 1 ELSE NULL END) AS  fifties,
+	COUNT(CASE FLOOR(EXTRACT(YEAR FROM dob) / 10) * 10 WHEN 1960 THEN 1 ELSE NULL END) AS  sixties
+FROM person
+WHERE father IS NOT NULL
+GROUP BY father
+HAVING COUNT(name) > 1
+
+ORDER BY name
+;
 
 
 -- Q3 returns (name,age)
@@ -28,24 +44,26 @@ WHERE dod IS NULL
 ORDER BY name
 ; 
 
--- Q4 returns (father,mother,child,born)------------------------------------------------------todo
-
---Gives correct except rank is over all families not separate families
-SELECT father, mother, name AS child, rank() over (ORDER BY dob) AS born 
+-- Q4 returns (father,mother,child,born)
+-- RANK() over (PARTITION BY <columns you want to group by> ORDER BY <columns you want to recieve a rank in that group>)
+SELECT father, mother, name AS child, RANK() over (PARTITION BY father, mother ORDER BY dob) AS born 
 FROM person
 WHERE mother IS NOT NULL OR father IS NOT NULL
 GROUP BY mother, father, name
-ORDER BY father, mother, born;
+ORDER BY father, mother, born
+;
 
 
 
 -- Q5 returns (father,mother,male)
 SELECT mother, father,
 	-- Taking advantage of the fact that COUNT doesn't count null values
-	 CAST(COUNT(CASE gender WHEN 'M' THEN 1 ELSE NULL END) * 1.0 / COUNT(gender) * 100.0 AS integer) AS male
+	 CAST(
+	 	COUNT(CASE gender WHEN 'M' THEN 1 ELSE NULL END) * 1.0 / COUNT(gender) * 100.0 AS INTEGER) AS male
 FROM person WHERE mother IS NOT NULL AND father IS NOT NULL
 GROUP BY mother, father
-ORDER BY mother, father;
+ORDER BY mother, father
+;
  
 
 -- Q6 returns (name)
