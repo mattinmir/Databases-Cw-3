8d66f3488fce27257a97311f45ebf837915460a9
mattinmir <mattinmir@users.noreply.github.com>
2015-02-26 18:15:54 +0000

    Update db_2015_cw3_template.sql

--- db_2015_cw3_template.sql
+++ db_2015_cw3_template.sql
@@ -12,12 +12,12 @@ HAVING COUNT(names) > 1
 ORDER BY popularity DESC, names
 ;
 
--- Q2 returns (name,forties,fifties,sixties)
+-- Q2 returns (name,forties,fifties,sixties)----------------------------------------------------todo
 SELECT 	mother,  
 	EXTRACT(YEAR FROM dob)
 FROM person
 WHERE mother IS NOT NULL
-GROUP BY mother
+GROUP BY mother, dob
 HAVING COUNT(name) > 1;
 
 
@@ -28,14 +28,30 @@ WHERE dod IS NULL
 ORDER BY name
 ; 
 
--- Q4 returns (father,mother,child,born)
-
+-- Q4 returns (father,mother,child,born)------------------------------------------------------todo
+SELECT father, mother, name AS child, RANK(dob)
+FROM person
+WHERE (mother IS NOT NULL OR father IS NOT NULL)
 ;
 
--- Q5 returns (father,mother,male)
+--------------------
+--Returns all children with at least one parent
+SELECT father, mother, name AS child, dob
+FROM person                                
+WHERE mother IS NOT NULL
+OR father IS NOT NULL
+ORDER BY father, mother;
 
+
+-- Q5 returns (father,mother,male)--------------------------------------------------------todo
+WHERE (SELECT DISTINCT gender FROM person) = 'M'
 ;
 
--- Q6 returns (name)
-
+-- Q6 returns (name)----------------------------------------------------------------------todo
+WHERE COUNT(SELECT DISTINCT gender FROM person) = 1
 ;
+
+SELECT ALL name, ALL father, ALL mother, DISTINCT gender
+FROM person AS families
+WHERE (father IS NOT NULL OR mother IS NOT NULL)
+AND 
