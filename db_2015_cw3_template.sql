d16a2c5fd85c46c31fab85e23fa7a2999c3b9248
mattinmir <mattinmir@users.noreply.github.com>
2015-02-26 16:36:43 +0000

    Update db_2015_cw3_template.sql

--- db_2015_cw3_template.sql
+++ db_2015_cw3_template.sql
@@ -2,26 +2,30 @@
 
 
 SELECT names AS name, COUNT(names) AS popularity
-FROM 
-	(
-	SELECT SPLIT_PART(name, ' ', 1) AS names
-	FROM person
-	)AS occurunces
+FROM
+    (
+		SELECT SPLIT_PART(name, ' ', 1) AS names
+		FROM person
+    )AS occurunces
 GROUP BY names
 HAVING COUNT(names) > 1
 ORDER BY popularity DESC, names
 ;
 
 -- Q2 returns (name,forties,fifties,sixties)
-SELECT mother, SUBSTRING(TO_CHAR(dob, 'YYYY-MM-DD') for 4) AS year
+SELECT 	mother,  
+	EXTRACT(YEAR FROM dob)
 FROM person
 WHERE mother IS NOT NULL
-GROUP BY mother, dob
-HAVING COUNT(name) > 1
-;
+GROUP BY mother
+HAVING COUNT(name) > 1;
+
 
 -- Q3 returns (name,age)
-
+SELECT name, EXTRACT(YEAR FROM AGE(CURRENT_DATE, dob)) AS age
+FROM person
+WHERE dod IS NULL
+ORDER BY name
 ; 
 
 -- Q4 returns (father,mother,child,born)
